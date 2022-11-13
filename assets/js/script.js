b9ca60f976662dec7fce86fb02456f48e5c174d5
nagibnur <nagibnur@hotmail.com>
2022-11-13 13:13:51 +0000

    created a submithandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const searchInput = document.querySelector('.input')
 const searchBtn = document.querySelector('.btn')
 
+searchBtn.addEventListener("click", submitHandler);
+
+function submitHandler(e) {
+    e.preventDefault();
+    const searchTerm = searchInput.value.trim();
+    
+}
+
 const Url =
 'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=15647aebbea9c6fe13b3adcfad1c1793'
 
