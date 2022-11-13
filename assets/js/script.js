a40d251a4f2bef29fe2a8a644c8b4fed332a3242
nagibnur <nagibnur@hotmail.com>
2022-11-13 14:25:56 +0000

    put data from api, test fail

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
 const searchInput = document.querySelector('.input')
 const searchBtn = document.querySelector('.btn')
+const card = document.querySelector('#card')
 
 searchBtn.addEventListener("click", submitHandler);
 
 function submitHandler(e) {
     e.preventDefault();
     const searchTerm = searchInput.value.trim();
-    
+    console.log(searchTerm);
+
+
+    const Url =
+    'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=15647aebbea9c6fe13b3adcfad1c1793'
+
+    fetch(Url)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        const weatherdata = [data.main, data.weather[0], data.wind]
+        for(let i = 0; i < weatherdata.length; i++) {
+            console.log(weatherdata[i]);
+        }
+
+        card.append(weatherdata)
+    })
 }
 
-const Url =
-'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=15647aebbea9c6fe13b3adcfad1c1793'
-
-fetch(Url)
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-    console.log(data);
-})
