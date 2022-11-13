c8d07c8da24d361259ad439a29564dd595abe6d3
nagibnur <nagibnur@hotmail.com>
2022-11-13 20:38:01 +0000

    extract data from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 const searchInput = document.querySelector('.input')
 const searchBtn = document.querySelector('.btn')
 const card = document.querySelector('#card')
+const li0 = document.querySelector('.li0')
+const li1 = document.querySelector('.li1')
+const li2 = document.querySelector('.li2')
+const li3 = document.querySelector('.li3')
+const li4 = document.querySelector('.li4')
+const li5 = document.querySelector('.li5')
 
 searchBtn.addEventListener("click", submitHandler);
 
@@ -8,7 +14,7 @@ function submitHandler(e) {
     e.preventDefault();
     const searchTerm = searchInput.value.trim();
     console.log(searchTerm);
-
+    card.hidden = false
 
     const Url =
     'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=15647aebbea9c6fe13b3adcfad1c1793'
@@ -18,12 +24,13 @@ function submitHandler(e) {
     return response.json();
     })
     .then(function (data) {
-        const weatherdata = [data.main, data.weather[0], data.wind]
-        for(let i = 0; i < weatherdata.length; i++) {
-            console.log(weatherdata[i]);
-        }
-
-        card.append(weatherdata)
+        li0.append(data.name, data.timezone)
+        li1.append(data.weather[0].icon)
+        li2.append(data.main.temp)
+        li3.append(data.weather[0].description)
+        li4.append(data.wind.speed)
+        li5.append(data.main.humidity)
+        
     })
 }
 
