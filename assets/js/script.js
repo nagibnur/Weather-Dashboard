bf96c22f55c69d9a7f6996499e7236926eeca796
nagibnur <nagibnur@hotmail.com>
2022-11-13 13:12:31 +0000

    got the api, test work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 const searchInput = document.querySelector('.input')
 const searchBtn = document.querySelector('.btn')
+
+const Url =
+'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=15647aebbea9c6fe13b3adcfad1c1793'
+
+fetch(Url)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+    console.log(data);
+})
