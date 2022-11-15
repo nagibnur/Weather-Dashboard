d73bb3d044e4bfb9d256ef273575384cc990d2e3
nagibnur <nagibnur@hotmail.com>
2022-11-15 17:47:20 +0000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 const searchInput = document.querySelector('.input')
 const searchBtn = document.querySelector('.btn')
-const card = document.querySelector('#card')
+const card = document.querySelector('#small')
 const li0 = document.querySelector('.li0')
-const li1 = document.querySelector('.li1')
+const iconImg = document.querySelector('.li1')
 const li2 = document.querySelector('.li2')
 const li3 = document.querySelector('.li3')
 const li4 = document.querySelector('.li4')
@@ -14,23 +14,37 @@ function submitHandler(e) {
     e.preventDefault();
     const searchTerm = searchInput.value.trim();
     console.log(searchTerm);
-    card.hidden = false
+    //card.hidden = false
 
     const Url =
-    'http://api.openweathermap.org/data/2.5/weather?q=london&APPID=15647aebbea9c6fe13b3adcfad1c1793'
+    'http://api.openweathermap.org/data/2.5/forecast?q='+searchInput.value+'&APPID=15647aebbea9c6fe13b3adcfad1c1793'
 
     fetch(Url)
     .then(function (response) {
     return response.json();
     })
     .then(function (data) {
-        li0.append(data.name, data.timezone)
-        li1.append(data.weather[0].icon)
-        li2.append(data.main.temp)
-        li3.append(data.weather[0].description)
-        li4.append(data.wind.speed)
-        li5.append(data.main.humidity)
-        
+        console.log(data);
+        cityName = data.city.name
+        icon = data.list[8].weather[0].icon
+        temp = data.list[8].main.temp
+        description = data.list[8].weather[0].description
+        windSpeed = data.list[8].wind.speed
+        humidity = data.list[8].main.humidity
+
+        li0.append(cityName)
+        iconImg.append(icon)
+        li2.append(temp)
+        li3.append(description)
+        li4.append(windSpeed)
+        li5.append(humidity)
     })
 }
+    // waether = {cityName, icon, temp, description, windSpeed, humidity}
+    // for(let i = 0; weather.lenght)
+
+   
+    
+
+
 
