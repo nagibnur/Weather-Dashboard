b9d334cebc509c2769b9a805dd84135e960206a1
nagibnur <nagibnur@hotmail.com>
2022-11-16 10:47:06 +0000

    fixed bugs, display extract data from api on inner html, added localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
 const searchInput = document.querySelector('.input')
 const searchBtn = document.querySelector('.btn')
-const card = document.querySelector('#small')
-const li0 = document.querySelector('.li0')
-const iconImg = document.querySelector('.li1')
-const li2 = document.querySelector('.li2')
-const li3 = document.querySelector('.li3')
-const li4 = document.querySelector('.li4')
-const li5 = document.querySelector('.li5')
+const cardContainerElement = document.querySelector('#card-Container')
+const currentDateContainer = document.querySelector('#currentDate-container')
 
 searchBtn.addEventListener("click", submitHandler);
 
 function submitHandler(e) {
     e.preventDefault();
     const searchTerm = searchInput.value.trim();
-    console.log(searchTerm);
-    //card.hidden = false
+    localStorage.setItem('search-term', searchTerm);
+    
 
     const Url =
     'http://api.openweathermap.org/data/2.5/forecast?q='+searchInput.value+'&APPID=15647aebbea9c6fe13b3adcfad1c1793'
@@ -24,27 +19,55 @@ function submitHandler(e) {
     return response.json();
     })
     .then(function (data) {
-        console.log(data);
         cityName = data.city.name
-        icon = data.list[8].weather[0].icon
-        temp = data.list[8].main.temp
-        description = data.list[8].weather[0].description
-        windSpeed = data.list[8].wind.speed
-        humidity = data.list[8].main.humidity
-
-        li0.append(cityName)
-        iconImg.append(icon)
-        li2.append(temp)
-        li3.append(description)
-        li4.append(windSpeed)
-        li5.append(humidity)
+            date = data.list[0].dt_txt
+            icon = data.list[0].weather[0].icon
+            temp = data.list[0].main.temp
+            description = data.list[0].weather[0].description
+            windSpeed = data.list[0].wind.speed
+            humidity = data.list[0].main.humidity
+            
+            currentDateContainer.innerHTML = `
+        <ul>
+            <li class="li0">${cityName}</li>
+            <li class="li11">${date}</li>
+            <img src="http://openweathermap.org/img/wn/${icon}.png" >
+            <li class="li2">temp: ${temp}</li>
+            <li class="li3">description: ${description}</li>
+            <li class="li4">windSpeed: ${windSpeed}</li>
+            <li class="li5">humidity: ${humidity}</li>
+        </ul>
+            `
+            
+        cardContainerElement.innerHTML = ''
+        for (let i = 0; i < 40; i = i + 8) {
+            cityName = data.city.name
+            date = data.list[i].dt_txt
+            icon = data.list[i].weather[0].icon
+            temp = data.list[i].main.temp
+            description = data.list[i].weather[0].description
+            windSpeed = data.list[i].wind.speed
+            humidity = data.list[i].main.humidity
+            const cardElement = document.createElement('div')
+            cardElement.className = 'card'
+            cardElement.innerHTML = `
+        <ul>
+            <li class="li0">${cityName}</li>
+            <li class="li11">${date}</li>
+            <img src="http://openweathermap.org/img/wn/${icon}.png" >
+            <li class="li2">temp: ${temp}</li>
+            <li class="li3">description: ${description}</li>
+            <li class="li4">windSpeed: ${windSpeed}</li>
+            <li class="li5">humidity: ${humidity}</li>
+        </ul>
+            `
+            cardContainerElement.append(cardElement)
+        }
     })
+    
 }
-    // waether = {cityName, icon, temp, description, windSpeed, humidity}
-    // for(let i = 0; weather.lenght)
+    
+   
 
    
-    
-
-
-
+  
